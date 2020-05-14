164e48bdb110a541b127a38abf80d077a3f69e44
Benkara Mohamed Tahar <tahar.benkara@ninjacoders-nc.com>
2020-05-14 05:45:26 +0300

    chapter-10. Creation dde content type entity.

---
+++ web/modules/custom/products/src/ProductListBuilder.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tahar
+ * Date: 14.05.2020
+ * Time: 03:36
+ */
+
+namespace Drupal\products;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * EntityListBuilderInterface implementation responsible for the Product entities.
+ */
+class ProductListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['id'] = $this->t('Product ID');
+    $header['name'] = $this->t('Name');
+    $header['source'] = $this->t('Source');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /* @var $entity \Drupal\products\Entity\Product */
+    $row['id'] = $entity->id();
+    $row['name'] = Link::fromTextAndUrl(
+      $entity->label(),
+      new Url(
+        'entity.product.canonical', [
+          'product' => $entity->id(),
+        ]
+      )
+    );
+    $row['source'] = $entity->getSource();
+    return $row + parent::buildRow($entity);
+  }
+
+}
